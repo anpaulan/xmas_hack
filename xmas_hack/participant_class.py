523ede8f26cf19c3e959379b6671e54c5cc81eaf
anpaulan <paulan21095@gmail.com>
2023-12-02 13:08:05 -0500

    fixed part_class

--- xmas_hack/participant_class.py
+++ xmas_hack/participant_class.py
@@ -5,22 +5,26 @@ class Participant:
         self.name = name
         self.assigned_recipient = None
         
-    def assign(self, lst):
-        remaining_participants = [p for p in lst if p != self]
-        self.assigned_recipient = random.choice(remaining_participants)
-        self.assigned_recipient.assigned_recipient = self
-
+    def assign(self, lst): 
+        # remaining_participants = [p for p in lst if p != self] # part of Ulises initial code, current is fixed version
+        remaining_participants = lst[:]
+        for participant in lst:
+            if participant != self:
+                if remaining_participants:
+                    participant.assigned_recipient = remaining_participants.pop()
+                
+    def forgot(self,name) :
+        user = input("What is your name?")
+        if user == self.name:
+            return self.assigned_recipient.name
+        
     def __str__(self):
         if self.assigned_recipient:
-            return f"🎅 {self.name} is Secret Santa for {self.assigned_recipient.name} "  # Fix: Change from 'self.secret_santa.name' to 'self.assigned_recipient.name'
+            return f"🎅 {self.name} is the Secret Santa for {self.assigned_recipient.name} "  # Fix: Change from 'self.secret_santa.name' to 'self.assigned_recipient.name'
         else:
             return f"🎅 {self.name} doesn't have a Secret Santa"
 
-def shuffle(lst):
-    for i in range(len(lst) - 1, 0, -1):
-        j = random.randint(0, i)
-        lst[i], lst[j] = lst[j], lst[i]
-
+    
 def main():
     participants = [
         Participant("Paul"),
@@ -30,13 +34,27 @@ def main():
         Participant("Asha"),
         Participant("Santa")
     ]
-
-    for participant in participants:
-        participant.assign(participants)
-
+    
+    forgetful = participants.forgot('Paul') #not working
+    
+    # final_list = dict()
     shuffle(participants)
     for participant in participants:
+        # final_list[participant] = participant.assign(participants) #Dictionary is not working as intended
         print(participant)
+    
+''' #this is me trying to make a dict so that people can be reminded of who their assignment is, doesnt work right now, and might not be worth putting time into this.
+    for part, assignments in final_list.items():
+        print(f'{part} - {assignments}')
+    
+    print(f'{final_list.items}')
+'''        
+
+def shuffle(lst):
+    for i in range(len(lst) - 1, 0, -1):
+        j = random.randint(0, i)
+        lst[i], lst[j] = lst[j], lst[i]
+
 
 if __name__ == "__main__":
     main()
