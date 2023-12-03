56e7debb2c96d9af51f8291185b5bf58eff4e7ce
Ulises_Sanch3z <chino287a@gmail.com>
2023-12-02 22:41:21 -0500

    added library.py

--- xmas_hack/app.py
+++ xmas_hack/app.py
@@ -6,39 +6,47 @@ from participant_class import shuffle, Participant
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    names = []
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/reveal', methods=['GET', 'POST'])
+def reveal():
+    participants = []
+
     if request.method == 'POST':
         user_input = request.form['user_input']
+
         if user_input:
-           
             new_participant_names = [name.strip() for name in user_input.split(',')]
-            
-           
+
             for name in new_participant_names:
                 new_participant = Participant(name)
-                names.append(new_participant)
+                participants.append(new_participant)
 
-                
+            remaining_participants = participants.copy()
 
-    if len(names) > 1:
-        shuffle(names)
-        for participant in names:
-            participant.assign(names)
-        
+            # for x in range(0, len(participants) -1):
+            #     participants[x].assign(participants[x+1])
+            #     if x == len(participants) -1:
+            #         participants[len(participants) -1].assign(participants[0])
+            # while len(remaining_participants) >= 1:
+            #     for participant in participants:
+            #         participant.assign(remaining_participants)
+            #         if participant.assigned_recipient in remaining_participants:
+            #             remaining_participants.remove(participant.assigned_recipient)
 
-    return render_template('reveal.html', participants=names)
+    return render_template('reveal.html', participants=participants)
 
-
-@app.route('/reveal')
-def reveal():
-    return render_template('reveal.html')
 
 @app.route('/timer')
 def timer():
     return render_template("timer.html")
 
+
+@app.route('/not_yet')
+def not_yet():
+    return render_template('not_yet.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
